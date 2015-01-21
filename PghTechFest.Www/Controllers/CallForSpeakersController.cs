fdcc9a2f1072bcd58a451e69d11c2bf8b33700b5
dustinupdyke <dustin@updyke.com>
2015-01-21 07:19:44 -0500

    Adds call for speakers stubs

--- PghTechFest.Www/Controllers/CallForSpeakersController.cs
+++ PghTechFest.Www/Controllers/CallForSpeakersController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using PghTechFest.Www.Models.Domain;
 using PghTechFest.Www.Models;
 
@@ -14,27 +7,6 @@ namespace PghTechFest.Www.Controllers
     public class CallForSpeakersController : Controller
     {
         private DatabaseContext db = new DatabaseContext();
-
-        // GET: /CallForSpeakers/
-        public ActionResult Index()
-        {
-            return View(db.Sessions.ToList());
-        }
-
-        // GET: /CallForSpeakers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Session session = db.Sessions.Find(id);
-            if (session == null)
-            {
-                return HttpNotFound();
-            }
-            return View(session);
-        }
 
         // GET: /CallForSpeakers/Create
         public ActionResult Create()
@@ -54,67 +26,15 @@ namespace PghTechFest.Www.Controllers
             {
                 db.Sessions.Add(session);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("thanks");
             }
 
             return View(session);
         }
 
-        // GET: /CallForSpeakers/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Thanks()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Session session = db.Sessions.Find(id);
-            if (session == null)
-            {
-                return HttpNotFound();
-            }
-            return View(session);
-        }
-
-        // POST: /CallForSpeakers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,Title,Description,LengthInMinutes")] Session session)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(session).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(session);
-        }
-
-        // GET: /CallForSpeakers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Session session = db.Sessions.Find(id);
-            if (session == null)
-            {
-                return HttpNotFound();
-            }
-            return View(session);
-        }
-
-        // POST: /CallForSpeakers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Session session = db.Sessions.Find(id);
-            db.Sessions.Remove(session);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            return View();
         }
 
         protected override void Dispose(bool disposing)
