2feb49cd5b14fec901ee4fb4b56fa5cd55ac1dd7
dustinupdyke <dustin@updyke.com>
2015-01-29 07:04:17 -0500

    Changing venue to place because azure hates venues

---
+++ PghTechFest.Www/Controllers/PlaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PghTechFest.Www.Controllers
+{
+    public class PlaceController : Controller
+    {
+        //
+        // GET: /Venue/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
