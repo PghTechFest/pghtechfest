81e22a2b3f4b5516d12d53ec882aa2356aaf7475
Kepes, Eric <eric.kepes@aesynt.com>
2015-05-19 08:55:46 -0400

    Sometimes people like to use you for a punching bag.

---
+++ PghTechFest.Www/Controllers/CoCController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PghTechFest.Www.Controllers
+{
+    public class CoCController : Controller
+    {
+        //
+        // GET: /CoC/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
